e34370ee8d1a174cd290842ee98d98c317020bc0
Celso Miranda <769237+celsomiranda@users.noreply.github.com>
2023-07-10 21:36:32 +0100

    Initial fumble with VPN

--- hosts/common/universal/networking.nix
+++ hosts/common/universal/networking.nix
@@ -1,8 +1,18 @@
 { config, ... }: 
 {
-  networking.hosts = {
-    "10.84.0.50" = ["svcads01.iscte-iul.pt"];
-    "10.84.0.51" = ["svcaroot01.iscte-iul.pt"];
-    "10.84.0.52" = ["svcasub01.iscte-iul.pt"];
+  networking = {
+    hosts = {
+      "10.84.0.50" = ["svcads01.iscte-iul.pt"];
+      "10.84.0.51" = ["svcaroot01.iscte-iul.pt"];
+      "10.84.0.52" = ["svcasub01.iscte-iul.pt"];
+    };
+
+    # openconnect.interfaces = {
+    #   iscte = {
+    #     gateway = "vpn.iscte-iul.pt";
+    #     protocol = "gp";
+    #     user = "cjcma";
+    #   };
+    # };
   };
 }
